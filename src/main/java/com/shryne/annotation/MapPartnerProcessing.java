93570d17dfeb737188ad177ee0ba272d82211bbe
= <=>
2021-01-25 09:35:44 +0100

    create processor

---
+++ src/main/java/com/shryne/annotation/MapPartnerProcessing.java
@@ -0,0 +1,20 @@
+package com.shryne.annotation;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+@SupportedAnnotationTypes("com.something.Processing.MapPartner")
+@SupportedSourceVersion(SourceVersion.RELEASE_0)
+public class MapPartnerProcessing extends AbstractProcessor {
+    @Override
+    public boolean process(
+        Set<? extends TypeElement> annotations, RoundEnvironment roundEnv
+    ) {
+        return false;
+    }
+}
